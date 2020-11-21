8a95502332f04d29e79158fd19f84451e70a4b45
khodarkhashi <afibuzzaman.anik@gmail.com>
2020-11-20 23:48:48 -0500

    lattitude over 40

--- rdd/airports/AirportsInUsaProblem.py
+++ rdd/airports/AirportsInUsaProblem.py
@@ -1,4 +1,15 @@
-from pyspark import SparkContext
+import sys
+sys.path.insert(0,'.')
+from pyspark import SparkContext, SparkConf
+from commons.Utils import Utils
+
+def splitComma(line: str):
+    splits = Utils.COMMA_DELIMITER.split(line)
+    return "{}, {}".format(splits[1],splits[2])
+
+def lat_country(line: str):
+    splits = Utils.COMMA_DELIMITER.split(line)
+    return "{}, {}".format(splits[2],splits[3])
 
 if __name__ == "__main__":
 
@@ -15,3 +26,18 @@ if __name__ == "__main__":
     "Dowagiac Municipal Airport", "Dowagiac"
     ...
     '''
+
+    conf = SparkConf().setAppName("kkAirports").setMaster("local[*]")
+    sc = SparkContext(conf = conf)
+
+    airports = sc.textFile("in/airports.text")
+    airportsInUSA = airports.filter(lambda line : Utils.COMMA_DELIMITER.split(line)[3] == "\"United States\"")
+
+    airportNameAndCityNames = airportsInUSA.map(splitComma)
+    airportNameAndCityNames.saveAsTextFile("out/airports_in_usa_kk")
+
+    lattitude = sc.textFile("in/airports.text")
+    lattitudeOverForty = lattitude.filter(lambda line : float(Utils.COMMA_DELIMITER.split(line)[6]) > 40)
+
+    lattitudeOverFortyNames = lattitudeOverForty.map(lat_country)
+    lattitudeOverFortyNames.saveAsTextFile("out/lattitude_over_forty_kk")
